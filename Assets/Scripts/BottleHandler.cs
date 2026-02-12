95b3f86418c73bb0133331e3770dd58e4637637f
Aaro <aaro.lehtoaho@student.oulu.fi>
2026-02-12 15:53:03 +0200

    Added logic for destroying bottle and spawning new one

--- Assets/Scripts/BottleHandler.cs
+++ Assets/Scripts/BottleHandler.cs
@@ -2,7 +2,10 @@ using UnityEngine;
 
 public class BottleHandler : MonoBehaviour
 {
+    public bool BottleHit = false;
+
     private GameObject[] bottles;
+    private int currentBottleIndex = -1;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,7 +22,18 @@ public class BottleHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (BottleHit)
+        {
+            IncreaseScore();
+            BottleHit = false;
+
+            int randomIndex = GetRandomBottleIndex();
+            if (randomIndex == currentBottleIndex)
+            {
+                randomIndex = (randomIndex + 1) % bottles.Length;
+            }
+            ActivateBottle(randomIndex);
+        }
     }
 
     private void ActivateBottle(int index)
@@ -27,11 +41,17 @@ public class BottleHandler : MonoBehaviour
         if (index >= 0 && index < bottles.Length)
         {
             bottles[index].SetActive(true);
+            currentBottleIndex = index;
         }
     }
 
     private int GetRandomBottleIndex()
     {
         return Random.Range(0, bottles.Length);
+    }
+
+    private void IncreaseScore()
+    {
+        // TODO
     }
 }
