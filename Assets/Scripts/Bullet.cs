70dc9700a6555155b7f532290dfa94f3633b288b
Aaro <aaro.lehtoaho@student.oulu.fi>
2026-02-17 09:36:09 +0200

    Adjusted bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,19 +2,21 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private int speed = 10;
+    private int speed = 20;
     private int lifeTime = 3;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        transform.Rotate(90, 0, 0, Space.Self);
+        transform.Translate(Vector3.up * 0.1f, Space.Self);
+        transform.Translate(Vector3.back * 0.07f, Space.Self);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * Time.deltaTime, Space.Self);
     }
 
     private void OnEnable()
@@ -27,7 +29,7 @@ public class Bullet : MonoBehaviour
         Destroy(gameObject);
     }
 
-    private void OnCollisionEnter(Collision other) {
+    private void OnTriggerEnter(Collider other) {
         bool isBottle = other.gameObject.CompareTag("Bottle");
         if (isBottle)
         {
